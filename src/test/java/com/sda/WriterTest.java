f6d30645da96892822b50b25ef23c5a9efb74375
Michal Madajewski <mw.madajewski@gmail.com>
2018-02-24 11:24:26 +0100

    capitalized named functionality

--- src/test/java/com/sda/WriterTest.java
+++ src/test/java/com/sda/WriterTest.java
@@ -1,13 +1,22 @@
 package com.sda;
 
+import com.sun.org.apache.regexp.internal.RE;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class WriterTest {
 
+    private Writer writer;
+
+    @Before
+    public void init() {
+        writer = new Writer();
+    }
+
     @Test
     public void sayHelloWithGivenNameTest() {
-        Writer writer = new Writer();
+    //    Writer writer = new Writer();
         String name = "Michal";
 
         String result = writer.sayHello(name);
@@ -17,7 +26,7 @@ public class WriterTest {
 
     @Test
     public void sayHelloWithNoNameSpecified() {
-        Writer writer = new Writer();
+    //    Writer writer = new Writer();
         String name = " ";
 
         String result = writer.sayHello("");
@@ -27,11 +36,20 @@ public class WriterTest {
 
     @Test
     public void sayHelloWithNullName() {
-        Writer writer = new Writer();
+    //    Writer writer = new Writer();
         String name = null;
 
         String result = writer.sayHello(name);
 
         Assert.assertEquals("Hello, my friend!", result);
+    }
+    @Test
+    public void sayHelloWithCapitalizedName() {
+    //    Writer writer = new Writer();
+        String name = "MICHAL";
+
+        String result = writer.sayHello(name);
+
+        Assert.assertEquals("HELLO, MICHAL!" , result);
     }
 }
